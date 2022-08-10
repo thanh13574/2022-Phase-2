b8df6883c7fc14fa30818309cc86d61a3e2a9309
thanh13574 <69949702+thanh13574@users.noreply.github.com>
2022-08-10 15:19:37 +1200

    part 1

--- 1. Frontend/my-app/src/App.tsx
+++ 1. Frontend/my-app/src/App.tsx
@@ -5,12 +5,10 @@ function App() {
   // Declare a new state variable, which we'll call "pokemonName"
   const [joke, setJoke] = useState("");
   const [jokeNumber, setJokeNumber] = useState("");
-  const [jokeInfo, setJokeInfo] = useState<undefined | any>(undefined);
-
 
   const axios = require("axios");
 
-  const options = 
+  const randoms = 
   {
     method: 'GET',
     url: 'https://webknox-jokes.p.rapidapi.com/jokes/search',
@@ -18,8 +16,18 @@ function App() {
       keywords: joke,
       numJokes: jokeNumber,
       category: '',
-      minRating: ''
+      minRating: '0'
     },
+    headers: {
+      'X-RapidAPI-Key': '77f96a77ffmshbf3de61e1538fddp15ae28jsn9b5ca1a6295e',
+      'X-RapidAPI-Host': 'webknox-jokes.p.rapidapi.com'
+    }
+  };
+
+  const param = {
+    method: 'GET',
+    url: 'https://webknox-jokes.p.rapidapi.com/jokes/random',
+    params: {minRating: '8', maxLength: '100'},
     headers: {
       'X-RapidAPI-Key': '77f96a77ffmshbf3de61e1538fddp15ae28jsn9b5ca1a6295e',
       'X-RapidAPI-Host': 'webknox-jokes.p.rapidapi.com'
@@ -31,7 +39,7 @@ function App() {
       <h1>Joke Databse</h1>
 
       <div>
-        <label>Keyword/ID:</label>
+        <label>Keyword/ID: </label>
         <input
           type="text"
           id="joke"
@@ -39,7 +47,7 @@ function App() {
           onChange={(e) => setJoke(e.target.value)}
         />
         <br/>
-        <label>Number of jokes:</label>
+        <label>Number of jokes: </label>
         <input
           type="int"
           id="joke_number"
@@ -47,16 +55,17 @@ function App() {
           onChange={(e) => setJokeNumber(e.target.value)}
         />
         <br/>
+        <br/>
         <button onClick={search}>Search</button>
+        <br/>
+        <button onClick={random}>Get a Random Joke</button>
       </div>
-
-      <p>You have entered {joke}</p>
     </div>
   );
 
   function search() {
     //@ts-ignore
-    axios.request(options).then(function (response) {
+    axios.request(param).then(function (response) {
       console.log(response.data);
       document.write("Found it!");
       document.write("<br>");
@@ -64,12 +73,31 @@ function App() {
 
       for(var num = 0; num < response.data.length; num++)
       {
-        var number = num +1;
-        document.write(number.toString());
-        document.write(" " + response.data[num].joke);
+        var number = num + 1;
+        document.write(number.toString() + ". ");
+        document.write("Joke ID: " + response.data[num].id + "<br>");
+        document.write(" " + response.data[num].joke + "<br> <br>");
+      }
+      //@ts-ignore
+    }).catch(function (error) {
+      console.error(error);
+      document.write("Joke not found");
+    });
+  }
+  function random() {
+    //@ts-ignore
+    axios.request(randoms).then(function (response) {
+      console.log(response.data);
+      document.write("Found it!");
+      document.write("<br>");
+      document.write("<br>");
 
-        document.write("<br>");
-        document.write("<br>"); 
+      for(var num = 0; num < response.data.length; num++)
+      {
+        var number = num + 1;
+        document.write(number.toString() + ". ");
+        document.write("Joke ID: " + response.data[num].id + "<br>");
+        document.write(" " + response.data[num].joke + "<br> <br>");
       }
       //@ts-ignore
     }).catch(function (error) {
